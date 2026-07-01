7c4de580fb7d76af7a0e42ba2d64378eb9a35a24
siddhi-ms <smsushir2023@gmail.com>
2026-07-01 16:01:55 +0530

    Implement role-based authorization

---
+++ src/middleware/roleMiddleware.js
@@ -0,0 +1,14 @@
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: 'Forbidden: insufficient permissions',
+      });
+    }
+
+    return next();
+  };
+};
+
+module.exports = authorize;
